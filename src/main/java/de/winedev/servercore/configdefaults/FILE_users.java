827f5ab9fd0c45aee7c7145e7e6a13be20b236db
wine97 <lucasmalkmus2@gmail.com>
2021-05-09 05:49:26 +0200

    Fixes

--- src/main/java/de/winedev/servercore/configdefaults/FILE_users.java
+++ src/main/java/de/winedev/servercore/configdefaults/FILE_users.java
@@ -3,19 +3,6 @@ package de.winedev.servercore.configdefaults;
 import de.winedev.servercore.framework.ConfigDefault;
 import de.winedev.servercore.framework.WGSFile;
 
-import java.util.ArrayList;
-
-/*
-users:
-  cce8457b-2667-4996-b56c-bad794725d42:
-    name: WineGaming
-    group: default
-    permissions:
-    - servercore.move
-    - servercore.chat
-
- */
-
 public class FILE_users extends ConfigDefault {
     public FILE_users(WGSFile wgsFile) {
         super(users);
