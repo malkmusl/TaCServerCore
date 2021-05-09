6296666b11e8ee9149a4f82a0e77df8ba8df5090
wine97 <lucasmalkmus2@gmail.com>
2021-05-09 05:50:34 +0200

    Fixes

--- src/main/java/de/winedev/servercore/configdefaults/FILE_users.java
+++ src/main/java/de/winedev/servercore/configdefaults/FILE_users.java
@@ -3,6 +3,19 @@ package de.winedev.servercore.configdefaults;
 import de.winedev.servercore.framework.ConfigDefault;
 import de.winedev.servercore.framework.WGSFile;
 
+import java.util.ArrayList;
+
+/*
+users:
+  cce8457b-2667-4996-b56c-bad794725d42:
+    name: WineGaming
+    group: default
+    permissions:
+    - servercore.move
+    - servercore.chat
+
+ */
+
 public class FILE_users extends ConfigDefault {
     public FILE_users(WGSFile wgsFile) {
         super(users);
@@ -12,7 +25,7 @@ public class FILE_users extends ConfigDefault {
     public void addDefaults() {
         add("users.cce8457b-2667-4996-b56c-bad794725d42.name", "WineGaming");
         add("users.cce8457b-2667-4996-b56c-bad794725d42.group", "default");
-        add("users.cce8457b-2667-4996-b56c-bad794725d42.autonick", true);
+        add("users.cce8457b-2667-4996-b56c-bad794725d42.autonick", false);
         ArrayList<String> exampleperms = new ArrayList<>();
         exampleperms.add("servercore.*");
         add("users.cce8457b-2667-4996-b56c-bad794725d42.permissions", exampleperms);
